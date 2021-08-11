ff909825f22676d6721909afaad43bb1cf9685da
zyk <565264599@qq.com>
2021-08-11 11:08:35 +0800

    数据库改为postgresql

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/mvc/controller/IndexController.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/mvc/controller/IndexController.java
@@ -1,18 +1,29 @@
-//package com.xxl.job.executor.mvc.controller;
-//
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//@EnableAutoConfiguration
-//public class IndexController {
-//
-//    @RequestMapping("/")
-//    @ResponseBody
-//    String index() {
-//        return "xxl job executor running.";
-//    }
-//
-//}
+package com.xxl.job.executor.mvc.controller;
+
+import com.xxl.job.core.executor.XxlJobExecutor;
+import com.xxl.job.core.handler.impl.MethodJobHandler;
+import com.xxl.job.executor.service.jobhandler.TestAddJob;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sun.jvm.hotspot.debugger.win32.coff.TestDebugInfo;
+
+import java.lang.reflect.Method;
+
+@Controller
+@EnableAutoConfiguration
+public class IndexController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        TestAddJob testAddJob = new TestAddJob();
+        Method[] methods = testAddJob.getClass().getDeclaredMethods();
+        methods[0].setAccessible(true);
+        MethodJobHandler methodJobHandler = new MethodJobHandler(testAddJob,methods[0],null,null);
+        XxlJobExecutor.registJobHandler("test111",methodJobHandler);
+        return "xxl job executor running.";
+    }
+
+}
---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/TestAddJob.java
@@ -0,0 +1,17 @@
+package com.xxl.job.executor.service.jobhandler;
+
+/**
+ * Description:
+ *
+ * @author {USER}
+ */
+public class TestAddJob {
+
+    public void testAddJob(){
+        for(int i = 0;i<100;i++){
+            System.out.println("123");
+        }
+    }
+
+
+}
